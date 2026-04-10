f0110cf7ae1e8a7556e8d0794ba1b0f95d2cfe15
yoyochen088 <somnolentcat@gmail.com>
2026-04-11 00:56:42 +0800

    Add files via upload

---
+++ members.js
@@ -0,0 +1,29 @@
+window.MEMBERS = [
+  {
+    "id": 1,
+    "gameId": "gt02q9a9f05",
+    "nickname": "Yoyo❤️",
+    "note": "會長"
+  },
+  {
+    "id": 2,
+    "gameId": "gt1eq8y2p0q",
+    "nickname": "長魚菀琪",
+    "note": "副會長",
+    "left": false
+  },
+  {
+    "id": 3,
+    "gameId": "gt036u0jwe0",
+    "nickname": "珞羽",
+    "note": "副會長",
+    "left": false
+  },
+  {
+    "id": 4,
+    "gameId": "gt01amfe756",
+    "nickname": "小貓曬太陽",
+    "note": "理事",
+    "left": false
+  }
+];
